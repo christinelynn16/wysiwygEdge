8fc08564830282e5eda18f4ea69b7f2efca4fb6e
Christine Anthony <canthony@leappoint.com>
2024-10-03 18:56:45 -0400

    Update fragment.js

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -25,6 +25,7 @@ export async function loadFragment(path) {
     if (resp.ok) {
 
       const json = await resp.json();
+      console.log("-------JSON--------> {}",json);
       
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
