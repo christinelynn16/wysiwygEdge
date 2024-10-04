28dac15fb26efdb078c28a81b0e4858f4cd438ff
Christine Anthony <canthony@leappoint.com>
2024-10-04 13:51:18 -0400

    Update fragment.js

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -20,22 +20,11 @@ import {
 export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
     // eslint-disable-next-line no-param-reassign
-    /* eslint-disable no-unused-vars */
-    //const jsonSt = '.json';
-    //const baseURL = document.location.origin;
-    //console.log(jsonSt);
-    //console.log(baseURL);
 
     path = path.replace(/(\.plain)?\.html/, '');
 
-    //const path2 = path.replace('/content/dam', '/api/assets');
-    //const newpath = '${baseURL}${path2}${jsonSt}';
-
     const resp = await fetch(`${path}.plain.html`);
-    //const resp2 = await fetch(`${path2}`);
-    //const resp3 = await fetch(`${newpath}`);
-    /* eslint-enable no-unused-vars */
-
+   
     if (resp.ok) {
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
