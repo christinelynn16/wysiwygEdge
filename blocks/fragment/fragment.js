f88187baac98be259bce974852639cb62412f434
Christine Anthony <canthony@leappoint.com>
2024-10-04 10:51:00 -0400

    Revert "Update fragment.js"
    This reverts commit 41758975572dba3df0d878c1b93e93f3a1f3d37a.

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -24,7 +24,7 @@ export async function loadFragment(path) {
     const resp = await fetch(`${path}.plain.html`);
     if (resp.ok) {
       const json = await resp.json();
-      console.log('-------JSON--------> ' + json.text);
+      console.log('-------JSON--------> ' + json);
 
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
