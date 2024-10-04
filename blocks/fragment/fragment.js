ccc1d4d2ed6b21019fbc69bd9ca3e8d41d404a5a
Christine Anthony <canthony@leappoint.com>
2024-10-04 11:00:52 -0400

    Update fragment.js

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -21,7 +21,7 @@ export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
     // eslint-disable-next-line no-param-reassign
     path = path.replace(/(\.plain)?\.html/, '');
-    var path2 = path.replace('/content/dam', '/api/assets') + '.json';
+    const path2 = path.replace('/content/dam', '/api/assets') + '.json';
     console.log(path2);
     
     const resp = await fetch(`${path}.plain.html`);
