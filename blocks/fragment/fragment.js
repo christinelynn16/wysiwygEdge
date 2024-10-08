51f20368f9091dfc3a221a48e259bf2716e8a02a
Christine Anthony <canthony@leappoint.com>
2024-10-08 14:00:21 -0400

    Update fragment.js

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -19,42 +19,10 @@ import {
  */
 export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
-    /* eslint-disable no-unused-vars */
     // eslint-disable-next-line no-param-reassign
     path = path.replace(/(\.plain)?\.html/, '');
-    const path2a = path.replace('/content/dam', '/api/assets');
-    const path2 = path2a.concat('.json');
-    const myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    const windowLocation = window.location.origin;
-    const response2 = await fetch(path2, {
-      headers: myHeaders,
-      baseURL: windowLocation,
-    });
-    const fullURL2 = `${windowLocation}${path2}`;
     const resp = await fetch(`${path}.plain.html`);
-    const resp2 = await fetch(fullURL2);
-    const resp3 = await fetch('https://author-p66217-e731910.adobeaemcloud.com/api/assets/wysiwygEdge/alex.json');
-    fetch('https://author-p66217-e731910.adobeaemcloud.com/api/assets/wysiwygEdge/alex.json')
-      .then((response) => {
-        if (response.redirected) {
-          console.log('Request was redirected to:', response.url);
-        }
-        const resp4 = response;
-      });
-    const query = 'query { employeefragmentList {items { employeeName, employeeTitle, employeeImage{ ... on ImageRef{ _path}}}}}';
-    // Define the AEM GraphQL endpoint
-    const endpoint = 'https://author-p66217-e731910.adobeaemcloud.com/content/cq:graphql/wysiwygEdge/endpoint';
-    // Use fetch to send the GraphQL query
-    const qlResp = fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query }), // Send the query as a JSON string
-    });
-    /* eslint-enable no-unused-vars */
-    if (resp.ok && resp2.ok) {
+    if (resp.ok) {
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
 
