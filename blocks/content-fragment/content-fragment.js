01436c312caef69fca3847c8ab85da0771de0c59
Christine Anthony <canthony@leappoint.com>
2024-10-08 17:55:50 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -39,7 +39,26 @@ export async function loadFragment(path) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       
-      const fragmentData = await response.json();
+      const resp = await response.json();
+
+      if (resp.ok) {
+        const main = document.createElement('main');
+        main.innerHTML = await resp.text();
+  
+        // reset base path for media to fragment base
+        const resetAttributeBase = (tag, attr) => {
+          main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+            elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
+          });
+        };
+        resetAttributeBase('img', 'src');
+        resetAttributeBase('source', 'srcset');
+  
+        decorateMain(main);
+        await loadBlocks(main);
+        return main;
+      }
+
     } catch (error) {
       console.error('Error fetching content fragment:', error);
       block.textContent = 'Error loading content fragment';
