c6c61fbcf81e1c4fecffc22cc30314d3e75008f4
Christine Anthony <canthony@leappoint.com>
2024-10-11 16:09:46 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -21,70 +21,30 @@ export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
     // eslint-disable-next-line no-param-reassign
     try {
-      const cfPath = '/assets/api/wysiwygEdge';
-      const cfPath2 = '/content/dam/wysiwygEdge';
-      const cfPath3 = '/content/dam';
       const query = new URLSearchParams({
         cursor: 'string',
         limit: '1',
-        path: cfPath,
-        references: 'direct-hydrated',
+        path: '/content/dam/wysiwygEdge',
+        references: 'direct-hydrated'
       }).toString();
-      // eslint-disable-next-line
-      const query2 = new URLSearchParams({
-        cursor: 'string',
-        limit: '1',
-        path: cfPath2,
-        references: 'direct-hydrated',
-      }).toString();
-      // eslint-disable-next-line
-      const query3 = new URLSearchParams({
-        cursor: 'string',
-        limit: '1',
-        path: cfPath3,
-        references: 'direct-hydrated',
-      }).toString();
+
       const bucket = 'author-p66217-e731910';
-      const token = 'eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LWF0LTEuY2VyIiwia2lkIjoiaW1zX25hMS1rZXktYXQtMSIsIml0dCI6ImF0In0.eyJpZCI6IjE3Mjg0ODg0NDUwNzVfMjA3ZTY1MDktZmRiNi00ZmVlLThlZjMtODU1ZGE4MzBmNmY2X3VlMSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiJjbS1wNjYyMTctZTczMTkxMC1pbnRlZ3JhdGlvbi0zIiwidXNlcl9pZCI6IjI1QTMxRTMxNjcwNTY4QUUwQTQ5NUZBMUB0ZWNoYWNjdC5hZG9iZS5jb20iLCJhcyI6Imltcy1uYTEiLCJhYV9pZCI6IjI1QTMxRTMxNjcwNTY4QUUwQTQ5NUZBMUB0ZWNoYWNjdC5hZG9iZS5jb20iLCJjdHAiOjAsImZnIjoiWTNKS1o1SDZGUFA1TUhVS0ZNUVZZSEFBVFE9PT09PT0iLCJtb2kiOiJiMDc3NTUxYiIsImV4cGlyZXNfaW4iOiI4NjQwMDAwMCIsInNjb3BlIjoicmVhZF9wYy5kbWFfYWVtX2FtcyxvcGVuaWQsQWRvYmVJRCxyZWFkX29yZ2FuaXphdGlvbnMsYWRkaXRpb25hbF9pbmZvLnByb2plY3RlZFByb2R1Y3RDb250ZXh0IiwiY3JlYXRlZF9hdCI6IjE3Mjg0ODg0NDUwNzUifQ.FohXYJox4Rhg1Q1-Umcv-M2okU1i3aNoUyD3gNPGNafmeSB26z-oNRWh_hJ-kSLQ3WCF_9k0lpd7XuVkFQFnuPB6PVCg6f9fHFUkiynsO-tWdEWJythjiWM-dxAXAzusm8hCLw3wqt2CmJHbzCAvVIX_ftausuNoqueJUhZBrxrwmWHN5k3w5yn93XMFAXh00qAM9tpSoNLwsAtaXRlknQmNyVXYOBPxBzTqXLbWHYNzusF7yHB2gy2hVOaTNSwBKSM4PUv3h40FsIt_Q_HBQeMFAnMwXywXaEGy2w4yy5f-muA7nxOP0yd_zUajJUht0bxMHffoWz0oPKCCq6TAmw'; // Replace with your actual token
-      const totalURL = `https://${bucket}.adobeaemcloud.com/adobe/sites/cf/fragments?${query}`;
-      // eslint-disable-next-line
-      const totalURL2 = `https://${bucket}.adobeaemcloud.com/adobe/sites/cf/fragments?${query}`;
-      // eslint-disable-next-line
-      const totalURL3 = `https://${bucket}.adobeaemcloud.com/adobe/sites/cf/fragments?${query3}`;
-      const response = await fetch(
-        `${totalURL}`,
+      const resp = await fetch(
+        `https://${bucket}.adobeaemcloud.com/adobe/sites/cf/fragments?${query}`,
         {
           method: 'GET',
           headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
+            Authorization: 'Bearer <YOUR_JWT_HERE>'
+          }
+        }
       );
-      // eslint-disable-next-line
-      const response2 = await fetch(
-        `${totalURL}`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-      // eslint-disable-next-line
-      const response3 = await fetch(
-        `${totalURL}`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-      const data = await response.json();
-      if (!response.ok && data.ok) {
+
+      const data = await resp.text();
+      console.log(data);
+      if (!data.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-      const resp = await response.json();
+      const resp2 = await response.json();
       if (resp.ok) {
         const main = document.createElement('main');
         main.innerHTML = await resp.text();
