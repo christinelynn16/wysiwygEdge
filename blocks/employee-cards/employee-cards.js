00758d2d20ff5139ed95229a0a5c6c3bedbe09bf
Christine Anthony <canthony@leappoint.com>
2024-10-23 13:42:11 -0400

    Update employee-cards.js

--- blocks/employee-cards/employee-cards.js
+++ blocks/employee-cards/employee-cards.js
@@ -1,4 +1,8 @@
 export default function decorate(block) {
   // eslint-disable-next-line
   const [clinicId] = block.children;
+  // eslint-disable-next-line
+  const i = 0;
+  // eslint-disable-next-line
+  const p = i+1;
 }
