bf33a77a158d575be86d358361d9d27f4c1bfa3f
Christine Anthony <canthony@leappoint.com>
2024-10-25 14:34:07 -0400

    Update employee-cards.js

--- blocks/employee-cards/employee-cards.js
+++ blocks/employee-cards/employee-cards.js
@@ -2,8 +2,7 @@ export async function loadEmployees(clinicId, main) {
   const bearerToken = 'eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LWF0LTEuY2VyIiwia2lkIjoiaW1zX25hMS1rZXktYXQtMSIsIml0dCI6ImF0In0.eyJpZCI6IjE3Mjk4NzcwODg4MTZfZDdiNjYzZGUtNDNlNy00MThkLWFmNTYtMWZkYTk5YTc2M2JmX3VlMSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiJjbS1wNjYyMTctZTczMTkxMC1pbnRlZ3JhdGlvbi0zIiwidXNlcl9pZCI6IjI1QTMxRTMxNjcwNTY4QUUwQTQ5NUZBMUB0ZWNoYWNjdC5hZG9iZS5jb20iLCJhcyI6Imltcy1uYTEiLCJhYV9pZCI6IjI1QTMxRTMxNjcwNTY4QUUwQTQ5NUZBMUB0ZWNoYWNjdC5hZG9iZS5jb20iLCJjdHAiOjAsImZnIjoiWTRXUko1SDZGUFA1TUhVS0ZNUVZZSEFBVFE9PT09PT0iLCJtb2kiOiI1M2MzMzlhNCIsImV4cGlyZXNfaW4iOiI4NjQwMDAwMCIsInNjb3BlIjoicmVhZF9wYy5kbWFfYWVtX2FtcyxvcGVuaWQsQWRvYmVJRCxyZWFkX29yZ2FuaXphdGlvbnMsYWRkaXRpb25hbF9pbmZvLnByb2plY3RlZFByb2R1Y3RDb250ZXh0IiwiY3JlYXRlZF9hdCI6IjE3Mjk4NzcwODg4MTYifQ.CzuslIN3NuRPXwQKXZ5ci5tiYIWrTEekUC-a_gelJ-C_UNdFtqlwNr0KQd7_cgFkBCwze56lgAZW3PKR1zPtey4qgd9vxy5Tjh-OxtJgcVh465FtA3l785bEoXPYR1G6rgAkTpUoysx1WgvyNjisCZRRDOcpwHieTwNCbLnHNhEn-y_T9-lV-kWRFd-hL8uiEJPU6dp_D_XiqIItaBWtbPCbuUKOsxIBNXy4_lUtYIyDGUzsu5_vucUnTXtfioiU8SE449T-EMG625XK5zZPc6CO_ab8Bcs6TJaYAQ9hhDIARxWT7UX-3hExiJ3px-P0Ou8DBee1EW-Ul9Kt7LgwPg';
 
   if (clinicId) {
-    const query2 = 'https://author-p66217-e731910.adobeaemcloud.com/graphql/execute.json/wysiwygEdge/getEmployeesById';
-    const variables = { clinicId };
+    const query2 = `https://author-p66217-e731910.adobeaemcloud.com/graphql/execute.json/wysiwygEdge/getEmployeesById;practiceID=${clinicId}`;
     try {
       const response2 = await fetch(query2, {
         method: 'GET',
@@ -11,9 +10,6 @@ export async function loadEmployees(clinicId, main) {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${bearerToken}`,
         },
-        body: JSON.stringify({
-          variables,
-        }),
       });
       // eslint-disable-next-line
       const dummy = 0;
